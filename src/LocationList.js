38804a3769f21c9c5b8cf846d4cffdbe105ea5ac
Riley Mathews <rileymathews80@gmail.com>
2018-05-29 11:52:47 -0500

    initial commit

---
+++ src/LocationList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Location from "./Location"
+
+class LocationList extends Component {
+    state = {
+        locations: [{
+            id: 0,
+            name: "Nashville North",
+            phone: "555-1212",
+            address: "10101 Binary Way"
+        },
+        {
+            id: 1,
+            name: "Nashville South",
+            phone: "555-1212",
+            address: "314 Pi Way"
+        }]
+    }
+
+
+    render() {
+        return (
+            <div className="content">
+                {this.state.locations.map(location => (
+                    <Location 
+                        name={location.name}
+                        address={location.address}
+                    />
+                ))}
+            </div>
+        )
+    }
+}
+
+export default LocationList
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Owner from "./Owner"
+import LocationList from "./LocationList"
+import "bulma/css/bulma.min.css"
+
+
+
+ReactDOM.render(<LocationList />, document.querySelector("#root"));
